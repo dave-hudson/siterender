e21bcb961d1df4e30fb5eb4c73e9c7b61f3533fb
Dave Hudson <dave-hudson@users.noreply.github.com>
2024-07-30 20:21:23 +0100

    Simplify configs

--- rollup.config.js
+++ rollup.config.js
@@ -5,27 +5,21 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: 'src/siterender.ts',
   output: {
-    file: 'build/siterender.mjs', // Use .mjs extension for ES module
+    file: 'build/siterender.mjs',
     format: 'esm',
-    sourcemap: true
+    sourcemap: true,
   },
   plugins: [
     resolve(),
     commonjs(),
     typescript({
-      tsconfig: './tsconfig.json'
+      tsconfig: './tsconfig.json',
     })
   ],
   external: [
-    'fs',
-    'path',
-    'os',
-    'http',
-    'https',
     'axios',
     'puppeteer',
-    'yargs',
-    'fast-xml-parser'
-  ]
+    'yargs'
+  ],
 };
 
